59ec9dc46a0dc404be7aebc9150c2f0233262506
Daniella Lopez <dannyanaislopez@gmail.com>
2015-03-24 18:18:55 -0600

    Initial Commit

---
+++ AGoodStart1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AGoodStart1
+//
+//  Created by lesdath on 3/24/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
